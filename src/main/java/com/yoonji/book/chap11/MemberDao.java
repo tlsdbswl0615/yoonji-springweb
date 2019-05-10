3f17d5d7037337c4b4ff27f80a811de717300c52
shin yoon ji-jdbc <tlsdbswl@naver.com>
2019-05-10 13:34:14 +0900

    0510

---
+++ src/main/java/com/yoonji/book/chap11/MemberDao.java
@@ -0,0 +1,17 @@
+package com.yoonji.book.chap11;
+
+	import java.util.List;
+
+	public interface MemberDao {
+
+	
+		Member selectByEmail(String email);
+
+		void insert(Member member);
+
+		void update(Member member);
+
+		List<Member> selectAll(int offset, int count);
+	
+		int countAll();
+	}
