e6783153875b926f52bb61386a9492d281e0bac3
shin yoon ji-jdbc <tlsdbswl@naver.com>
2019-05-24 14:09:08 +0900

    0524

--- src/main/java/com/yoonji/article/ArticleDao.java
+++ src/main/java/com/yoonji/article/ArticleDao.java
@@ -12,12 +12,12 @@ import org.springframework.stereotype.Repository;
 public class ArticleDao {
 
 	static final String LIST_ARTICLES = "select articleId, title, userId, name, left(cdate,16) cdate from article order by articleId desc limit ?,?";
-
 	static final String COUNT_ARTICLES = "select count(articleId) from article";
-
 	static final String GET_ARTICLE = "select articleId, title, content, userId, name, left(cdate,16) cdate, udate from article where articleId=?";
-
 	static final String ADD_ARTICLE = "insert article(title,content,userId,name) values(?,?,?,?)";
+	//게시글수정과삭제
+	static final String UPDATE_ARTICLE = "UPDATE article SET title=?, content=? WHERE articleId=?";
+	static final String DELETE_ARTICLE = "DELETE FROM article WHERE articleId=?";
 
 	@Autowired
 	JdbcTemplate jdbcTemplate;
@@ -25,34 +25,36 @@ public class ArticleDao {
 	RowMapper<Article> articleRowMapper = new BeanPropertyRowMapper<>(
 			Article.class);
 
-	/**
-	 * 글목록
-	 */
+	
 	public List<Article> listArticles(int offset, int count) {
 		return jdbcTemplate.query(LIST_ARTICLES, articleRowMapper, offset,
 				count);
 	}
 
-	/**
-	 * 글 목록 건수
-	 */
+	
 	public int getArticlesCount() {
 		return jdbcTemplate.queryForObject(COUNT_ARTICLES, Integer.class);
 	}
 
-	/**
-	 * 글조회
-	 */
+	
 	public Article getArticle(String articleId) {
 		return jdbcTemplate.queryForObject(GET_ARTICLE, articleRowMapper,
 				articleId);
 	}
 
-	/**
-	 * 글등록
-	 */
 	public int addArticle(Article article) {
 		return jdbcTemplate.update(ADD_ARTICLE, article.getTitle(),
 				article.getContent(), article.getUserId(), article.getName());
+		
+
+	}
+	//수정
+	public int updateArticle(Article article) {
+		return jdbcTemplate.update(UPDATE_ARTICLE, article.getTitle(),
+				article.getContent(), article.getArticleId());
+	}
+	//글삭제
+	public int deleteArticle(String articleId) {
+		return jdbcTemplate.update(DELETE_ARTICLE, articleId);
 	}
 }
