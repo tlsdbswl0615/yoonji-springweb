ea60d4d1f2f89fc4a51c68e8e016f027c16c48b8
shin yoon ji-jdbc <tlsdbswl@naver.com>
2019-05-24 13:39:22 +0900

    0524

--- src/main/java/com/yoonji/article/ArticleController.java
+++ src/main/java/com/yoonji/article/ArticleController.java
@@ -5,13 +5,15 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import com.yoonji.book.chap11.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import com.yoonji.book.chap11.Member;
 
 @Controller
 public class ArticleController {
@@ -49,31 +51,17 @@ public class ArticleController {
 		model.addAttribute("article", article);
 	}
 
-	/**
-	 * 글 등록 화면
-	 */
+	
 	@GetMapping("/article/addForm")
 	public String articleAddForm(HttpSession session) {
-		Object memberObj = session.getAttribute("MEMBER");
-		if (memberObj == null)
-			// 세션에 MEMBER가 없을 경우 로그인 화면으로
-			return "login/loginForm";
-
+		
 		return "article/addForm";
 	}
 
-	/**
-	 * 글 등록
-	 */
-	@PostMapping("/article/add")
-	public String articleAdd(Article article, HttpSession session) {
-		// 세션에 MEMBER가 없을 경우 로그인 화면으로
-		Object memberObj = session.getAttribute("MEMBER");
-		if (memberObj == null)
-			// 세션에 MEMBER가 없을 경우 로그인 화면으로
-			return "login/loginForm";
 
-		Member member = (Member) memberObj;
+	@PostMapping("/article/add")
+	public String articleAdd(Article article, 
+			@SessionAttribute("MEMBER")Member member) {
 		article.setUserId(member.getMemberId());
 		article.setName(member.getName());
 		articleDao.addArticle(article);
